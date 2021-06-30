74177bcddab78f3e978ada71df496bcc93b727bf
lidan <2364752564@qq.com>
2021-06-30 09:32:02 +0800

    准备注册后台用户命令

---
+++ application/admin/controller/Base.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace app\admin\controller;
+
+use think\Controller;
+
+class Base extends Controller
+{
+  /**
+   * Undocumented function
+   * @todo 用户鉴权
+   * @return void
+   * @author john <email@email.com>
+   */
+  public function index()
+  {
+    # code...
+  }
+
+  public function base()
+  {
+  }
+}
